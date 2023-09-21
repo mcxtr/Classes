8452f603a1c647fd7db8f8d18be20a721859906c
mcxtr <soloalish@gmail.com>
2023-09-21 17:33:54 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Alymbek";
+        student.lastName = "Temikhanow";
+        student.age = 16;
+        System.out.println(student.name+"\n"+student.lastName+"\n"+student.age+"\n");
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,5 @@
+public class Student {
+    String name;
+    String lastName;
+    int age;
+}
